a76b9e7bc1e1a5a6e1986a8a372a98e91087ca5f
success hycenth <successhycenth8@@gmail.com>
2022-08-06 01:42:28 +0100

    pushing success

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
-// import Header from "../layout/Header";
+import Header from "../layout/Header";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import "aos/dist/aos.css";
-import dynamic from "next/dynamic";
-
-const Header = dynamic(() => import("../layout/Header"), { ssr: false });
 
 function MyApp({ Component, pageProps, router }) {
   return (
