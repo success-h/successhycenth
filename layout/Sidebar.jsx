f74d825cd232103091f8a15f7bec9ed22b93d713
Success Hycenth <successhycenth8@gmail.com>
2022-09-27 08:57:31 +0100

    fixing nav aniations

--- layout/Sidebar.jsx
+++ layout/Sidebar.jsx
@@ -11,28 +11,35 @@ const Sidebar = ({ menu, setMenu }) => {
           menu ? "translate-x-32 " : "translate-x-full"
         }`}
       >
-        <div className="text-4xl font-Ranchers dark:text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-sky-600 cursor-pointer">
+        <div
+          data-aos="fade-left"
+          className="text-4xl font-Ranchers dark:text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-sky-600 cursor-pointer"
+        >
           <Link href="/#">SUCCESS</Link>
         </div>
         <div
+          data-aos="fade-left"
           onClick={() => setMenu(!menu)}
           className="my-4 dark:text-gray-200 mt-10 text-xl"
         >
           <a href="/#about">About</a>
         </div>
         <div
+          data-aos="fade-left"
           onClick={() => setMenu(!menu)}
           className="my-4  dark:text-gray-200 text-xl"
         >
           <a href="/#projects">Projects</a>
         </div>
         <div
+          data-aos="fade-left"
           onClick={() => setMenu(!menu)}
           className="my-4 dark:text-gray-200 text-xl"
         >
           <a href="/#technologies">Technologies</a>
         </div>
         <div
+          data-aos="fade-left"
           onClick={() => setMenu(!menu)}
           className="my-4 dark:text-gray-200 text-xl"
         >
--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,14 @@ import { ThemeProvider } from "next-themes";
 import "aos/dist/aos.css";
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("../layout/Header"), { ssr: false });
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
 function MyApp({ Component, pageProps, router }) {
+  useEffect(() => {
+    AOS.init({ duration: 1000, asing: "ease-out-cubic" });
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <ThemeProvider enableSystem={true} attribute="class">
