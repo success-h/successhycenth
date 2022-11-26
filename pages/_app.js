0b7c7b4106cf1e9d9d1045fe121730d20b904381
Success Hycenth <successhycenth8@gmail.com>
2022-11-26 20:45:49 +0100

    update app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 // import Header from "../layout/Header";
-import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
-import "aos/dist/aos.css";
-import dynamic from "next/dynamic";
-const Header = dynamic(() => import("../layout/Header"), { ssr: false });
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect, useState } from "react";
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import 'aos/dist/aos.css';
+import dynamic from 'next/dynamic';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect, useState } from 'react';
+const Header = dynamic(() => import('../layout/Header'), { ssr: false });
 function MyApp({ Component, pageProps, router }) {
   useEffect(() => {
-    AOS.init({ duration: 1000, asing: "ease-out-cubic" });
+    AOS.init({ duration: 1000, asing: 'ease-out-cubic' });
     AOS.refresh();
   }, []);
   return (
